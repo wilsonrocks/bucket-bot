802b8535779d6c6f4a34a60396f6d3c00726a728
James Wilson <jameswilson@hey.com>
2025-12-10 07:04:36 +0000

    tweak how lambda is built etc, deploy script still to come

--- backend/app.ts
+++ backend/app.ts
@@ -2,6 +2,10 @@ import { Kysely, PostgresDialect } from "kysely";
 import type { DB } from "kysely-codegen";
 import { Pool } from "pg";
 import Koa from "koa";
+import Router from "@koa/router";
+import fs from "fs";
+
+// const ca = fs.readFileSync("./certs/ca.pem").toString();
 
 const db = new Kysely<DB>({
   dialect: new PostgresDialect({
@@ -18,13 +22,21 @@ async function getRows() {
 
 const app = new Koa();
 
+const router = new Router({ prefix: "/v1" });
+
+router.get("/tourney", async (ctx) => {
+  const rows = await getRows();
+  ctx.body = { fakeData: rows };
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
 app.use(async (ctx) => {
-  if (ctx.path === "/tourney") {
-    const rows = await getRows();
-    ctx.body = rows;
-  } else {
-    ctx.body = "Hello World";
-  }
+  ctx.body;
+});
+
+app.use(async (ctx) => {
+  ctx.body = "I am bUKet bot for your uk malifaux rankings needs!\n";
 });
 
 export default app;
--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,19 +1,48 @@
 import * as cdk from "aws-cdk-lib/core";
 import { Construct } from "constructs";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as lambda from "aws-cdk-lib/aws-lambda";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    if (!process.env.DATABASE_URL) {
+      throw new Error("DATABASE_URL environment variable is not set");
+    } // TODO probably a neater way to do this inc typescript
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'InfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const backendLambda = new lambda.Function(this, "BucketBotHandler", {
+      code: lambda.Code.fromAsset("../backend/dist"),
+      handler: "handler.handler",
+      runtime: lambda.Runtime.NODEJS_22_X,
+      environment: {
+        DATABASE_URL: process.env.DATABASE_URL,
+        NODE_EXTRA_CA_CERTS: "/var/task/certs/ca.pem", // make sure this is copied across
+      },
+    });
+
+    // API Gateway
+    const api = new apigateway.RestApi(this, "BucketBotApi", {
+      restApiName: "Bucket Bot Service",
+      description: "API Gateway for Bucket Bot backend.",
+    });
+
+    // /v1 resource
+    const v1 = api.root.addResource("v1");
+    v1.addProxy({
+      defaultIntegration: new apigateway.LambdaIntegration(backendLambda),
+      anyMethod: true,
+    });
 
     // Tag all resources in this stack with the project name
     cdk.Tags.of(this).add("project", "Bucket Bot");
+
+    // Output the API Gateway URL
+    new cdk.CfnOutput(this, "ApiUrl", {
+      value: api.url,
+      description: "URL of the API Gateway endpoint",
+      exportName: "BucketBotApiUrl",
+    });
   }
 }
