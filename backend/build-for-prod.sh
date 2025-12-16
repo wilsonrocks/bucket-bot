8eb2a45cb315ce2cd1326aec4166dfc72cc91c41
James Wilson <jameswilson@hey.com>
2025-12-16 06:24:45 +0000

    remove attempt at actions, write an overarching script

--- backend/build-for-prod.sh
+++ backend/build-for-prod.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e
-echo "Installing dependencies..."
-npm ci
 echo "Building backend for production..."
 npx tsc
 cp package*.json dist
@@ -9,24 +7,7 @@ cd dist
 mkdir -p certs
 echo "Copying CA certificate..."
 cp ../../certs/ca.pem certs
-
-
-
-
-# OLD:
-
-# #!/bin/bash
-# set -e
-# echo 'Installing Typescript compiler...'
-# npm install typescript
-# echo "Building backend for production..."
-# npx tsc
-# cp package*.json dist
-# cd dist
-# mkdir -p certs
-# echo "Copying CA certificate..."
-# cp ../../certs/ca.pem certs
-# echo 'removing previous dependencies...'
-# rm -rf node_modules
-# echo "Installing production dependencies..."
-# npm ci
+echo 'removing previous dependencies...'
+rm -rf node_modules
+echo "Installing production dependencies..."
+npm ci
