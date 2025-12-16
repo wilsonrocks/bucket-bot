8c5b56cf96190d9b85d14cc9d25ef6254e4afcf7
James Wilson <jameswilson@hey.com>
2025-12-16 06:14:56 +0000

    rearrange build script

--- backend/build-for-prod.sh
+++ backend/build-for-prod.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
-echo 'Installing Typescript compiler...'
-npm install typescript
+echo "Installing dependencies..."
+npm ci
 echo "Building backend for production..."
 npx tsc
 cp package*.json dist
@@ -9,7 +9,24 @@ cd dist
 mkdir -p certs
 echo "Copying CA certificate..."
 cp ../../certs/ca.pem certs
-echo 'removing previous dependencies...'
-rm -rf node_modules
-echo "Installing production dependencies..."
-npm ci
+
+
+
+
+# OLD:
+
+# #!/bin/bash
+# set -e
+# echo 'Installing Typescript compiler...'
+# npm install typescript
+# echo "Building backend for production..."
+# npx tsc
+# cp package*.json dist
+# cd dist
+# mkdir -p certs
+# echo "Copying CA certificate..."
+# cp ../../certs/ca.pem certs
+# echo 'removing previous dependencies...'
+# rm -rf node_modules
+# echo "Installing production dependencies..."
+# npm ci
