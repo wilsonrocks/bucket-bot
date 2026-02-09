82c1e2eebef1ca757acfc22ec78a0a2b1a1e6f31
James Wilson <jameswilson@hey.com>
2026-02-09 10:01:19 +0000

    add network indicators

---
+++ frontend/src/components/network-indicator.tsx
@@ -0,0 +1,15 @@
+import { useIsFetching, useIsMutating } from '@tanstack/react-query'
+import { Group } from '@mantine/core'
+import { IconDownload, IconUpload } from '@tabler/icons-react'
+
+export const NetworkIndicator = () => {
+  const isFetching = useIsFetching()
+  const isMutating = useIsMutating()
+
+  return (
+    <Group>
+      {isFetching && <IconDownload />}
+      {isMutating && <IconUpload />}
+    </Group>
+  )
+}
