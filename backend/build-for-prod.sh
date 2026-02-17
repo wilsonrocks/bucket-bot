17087f1f03906473d4d8dec653705358ab87c2e2
James Wilson <jameswilson@hey.com>
2026-02-17 23:20:19 +0000

    copy CA cert _first_

--- backend/build-for-prod.sh
+++ backend/build-for-prod.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 set -e
 echo "Building backend for production..."
+mkdir -p certs
+echo "Copying CA certificate..."
+cp ../../certs/ca.pem certs
 npx kysely-codegen
 npx tsc
 cp package*.json dist
 cd dist
-mkdir -p certs
-echo "Copying CA certificate..."
-cp ../../certs/ca.pem certs
 echo 'removing previous dependencies...'
 rm -rf node_modules
 echo "Installing production dependencies..."
