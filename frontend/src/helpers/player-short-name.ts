50a26b6ab8d77eb15dfbecfe8064166502826f38
James Wilson <jameswilson@hey.com>
2026-04-04 20:39:43 +0100

    use slightly better template for names (#52)

--- frontend/src/helpers/player-short-name.ts
+++ frontend/src/helpers/player-short-name.ts
@@ -8,7 +8,7 @@ export function playerShortName({
   short_name,
 }: PlayerShortNameArgs): string {
   if (short_name) return short_name
-  const firstWord = name.split(' ')[0]
-  if (firstWord !== name) return firstWord
-  return name.slice(0, 8)
+  const words = name.split(' ')
+  if (words.length === 1) return words[0].slice(0, 10)
+  return words[0].slice(0, 9) + ' ' + words[1].slice(0, 1)
 }
