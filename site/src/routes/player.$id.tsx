fa5f33b49c6d3969304b7b61bf28cbd6b0c890dc
James Wilson <jameswilson@hey.com>
2026-09-17 09:04:49 +0100

    size player events table to its content so Event column isn't stretched
    Fixes #125

--- site/src/routes/player.$id.tsx
+++ site/src/routes/player.$id.tsx
@@ -158,7 +158,7 @@ function RouteComponent() {
         </Tabs.List>
 
         <Tabs.Panel value="events">
-          <table className="min-w-full text-sm tabular-nums">
+          <table className="text-sm tabular-nums">
             <thead>
               <tr className="border-b border-border text-left">
                 <th className="px-2 py-2 font-semibold">Event</th>
